fbb9ad1e7e6d848886f729e9a4ab76e094a46a84
Ahmed Hamdy <94504141+Ahmed-Hamdy101@users.noreply.github.com>
2023-12-14 16:18:15 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,36 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', 'App\Http\Controllers\HomeController@index');
+
+Route::get('posts/', 'App\Http\Controllers\PostControllers@index');
+
+Route::get('contact/', 'App\Http\Controllers\ContactController@index');
+
+// // create 
+Route::get('posts/create', 'App\Http\Controllers\PostControllers@create');
+
+
+Route::post('posts/store', 'App\Http\Controllers\PostControllers@store');
+
+// for get  or show the data on url
+Route::get('posts/{id}', 'App\Http\Controllers\PostControllers@show');
+
+// // for get  or show the data on url
+Route::get('posts/{id}/edit', 'App\Http\Controllers\PostControllers@edit');
+
+// save it  or update it
+Route::put('posts/{id}', 'App\Http\Controllers\PostControllers@update');
+
+//  delete it  
+Route::get('posts/{id}/delete', 'App\Http\Controllers\PostControllers@destory');
+
+// // index catagory 
+
+Route::get('catagory/', 'App\Http\Controllers\CatagoryController@index');
+
+Route::get('catagory/create', 'App\Http\Controllers\CatagoryController@create');
+
+// create catagory
+Route::post('/catagory/store', 'App\Http\Controllers\CatagoryController@store');
